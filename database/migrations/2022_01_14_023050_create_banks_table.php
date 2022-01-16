61809fc017ef067411728a2aabbf095a4cfbe560
HusnulM <husnulmub@gmail.com>
2022-01-16 12:22:45 +0700

    first commit

---
+++ database/migrations/2022_01_14_023050_create_banks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->string('bankid',10);
+            $table->string('bankname',100);
+            $table->timestamps();
+
+            $table->primary(['company_id', 'material']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banks');
+    }
+}
